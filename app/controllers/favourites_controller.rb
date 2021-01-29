44818401313def9adf501d105c449e593a940eb6
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-29 14:57:10 +0200

    Fix rubocop lint errors

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,5 +1,5 @@
 class FavouritesController < ApplicationController
-  before_action :set_favourite, only: [:show, :update, :destroy]
+  before_action :set_favourite, only: %i[show update destroy]
 
   # GET /favourites
   def index
@@ -39,13 +39,14 @@ class FavouritesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_favourite
-      @favourite = Favourite.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def favourite_params
-      params.require(:favourite).permit(:user_id, :accommodation_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_favourite
+    @favourite = Favourite.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def favourite_params
+    params.require(:favourite).permit(:user_id, :accommodation_id)
+  end
 end
