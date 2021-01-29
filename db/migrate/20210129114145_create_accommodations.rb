af24c38015f7254e5f6d981e93de8d498ad34dad
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-29 13:54:37 +0200

    Add accommodations migrations

---
+++ db/migrate/20210129114145_create_accommodations.rb
@@ -0,0 +1,14 @@
+class CreateAccommodations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :accommodations do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.float :price
+      t.integer :rooms
+      t.text :description
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
