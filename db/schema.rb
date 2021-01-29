cfa79bb4e54ca6f40f0b87d50ef65412cd1193d3
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-29 13:54:13 +0200

    Add accommodations migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_105033) do
+ActiveRecord::Schema.define(version: 2021_01_29_114145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accommodations", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.float "price"
+    t.integer "rooms"
+    t.text "description"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_accommodations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_01_29_105033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accommodations", "users"
 end
