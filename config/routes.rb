be066f4443716baae30fcc253fbfd08dc7a962e1
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-29 16:22:33 +0200

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: "favourites#index"
   resources :favourites, only: [:index]
   resources :accommodations
   devise_for :users
