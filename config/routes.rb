c57c36bb226557585a3ccc7ce46b3801c3b4103a
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-31 09:48:21 +0200

    Add accommodations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :favourites
-  resources :accommodations
+  resources :favourites, only: [:index]
+  resources :accommodations do
+    resources :favourites
+  end
   resources :users
   root to: "favourites#index"
 end
