cc2d2a0995b44da11b8562ddb4c977d182700b12
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-31 22:01:47 +0200

    Add validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  # password encryption
   has_secure_password
 
+  # validations
+  validates :username, presence: true, uniqueness: true, length: { in: 3..50 }
+  validates :email, presence: true, uniqueness: true, length: { in: 3..50 }
+  validates :password, presence: true, length: { in: 3..50 }
+
+  # associations
   has_many :accommodations
   has_many :favourites
 end
